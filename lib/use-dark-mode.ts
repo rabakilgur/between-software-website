c54e7b8554038a062e2e3473c4dfb3b032164058
Robin Garbe <mail@robin-garbe.de>
2022-05-10 19:44:42 +0200

    Added light and dark themes

---
+++ lib/use-dark-mode.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function useDarkMode(): ["dark"|"light", React.Dispatch<React.SetStateAction<string>>] {
+	const [theme, setTheme] = useState(
+		typeof window !== "undefined" ? String(localStorage.theme) : "dark"
+	);
+	const colorTheme = theme === "dark" ? "light" : "dark";
+
+	useEffect(() => {
+		const root = window.document.documentElement;
+
+		root.classList.remove(colorTheme);
+		root.classList.add(theme);
+
+		if (typeof window !== "undefined") localStorage.setItem("theme", theme);
+	}, [theme]);
+
+	return [colorTheme, setTheme];
+}
+
+export default useDarkMode;
