78b7b609cface43571000e98aa5eb4186345f44c
Robin Garbe <mail@robin-garbe.de>
2022-05-11 14:25:44 +0200

    Removed Ant Design and added Bootstrap 5

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type NextPage from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
 import { Button, PageHeader } from "antd";
 import useDarkMode from "../lib/use-dark-mode";
