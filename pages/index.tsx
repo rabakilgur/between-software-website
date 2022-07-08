3afb871b1952993a4bc83ab21f0af695343b23ce
Robin Garbe <mail@robin-garbe.de>
2022-07-08 02:10:40 +0200

    Added very basic demo grapic to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import { Container } from "react-bootstrap";
 import FrontPage from "../components/frontpage/frontpage";
 import Header from "../components/header/header";
 import ThemeToggle from "../components/theme-toggle/theme-toggle";
@@ -13,6 +15,9 @@ const Home: NextPage = () => {
 			{/* <FrontPage /> */}
 			<Header />
 			<span id="top"></span>
+			<Container className="position-relative">
+				<img src="./between_frontpage_bg.png" width="60%" className="position-absolute end-0 m-3" />
+			</Container>
 		</>
 	);
 };
